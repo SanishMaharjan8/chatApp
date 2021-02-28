6a07b8bb29dff037917b8b98398e5caa7b86ee36
Sanish Maharjan <crazy.san8@gmail.com>
2021-02-28 17:06:40 +1100

    main tab navigator

---
+++ navigation/index.tsx
@@ -0,0 +1,61 @@
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
+import * as React from 'react';
+import { ColorSchemeName, View } from 'react-native';
+//import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Colors from '../constants/Colors';
+
+import {Octicons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import NotFoundScreen from '../screens/NotFoundScreen';
+import { RootStackParamList } from '../types';
+import MainTabNavigator from './MainTabNavigator';
+import LinkingConfiguration from './LinkingConfiguration';
+
+// If you are not familiar with React Navigation, we recommend going through the
+// "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
+export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+  return (
+    <NavigationContainer
+      linking={LinkingConfiguration}
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+// A root stack navigator is often used for displaying modals on top of all other content
+// Read more here: https://reactnavigation.org/docs/modal
+const Stack = createStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ 
+      headerStyle:{
+        backgroundColor: Colors.light.tint,
+        shadowOpacity: 0,
+        elevation: 0
+      },
+      headerTintColor: Colors.light.background,
+      headerTitleAlign: 'left',
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+      }}>
+      <Stack.Screen 
+        name="Root" 
+        component={MainTabNavigator}
+        options={{
+          title:"ChatApp",
+          headerRight: () => {
+              return <View style={{flexDirection:"row", width:60, marginRight:10, justifyContent:"space-between" }}>
+              <Octicons name="search" size={20} color="white" />
+              <MaterialCommunityIcons name="dots-vertical" size={20} color="white" />
+            </View>;
+          }
+        }}
+       />
+      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+    </Stack.Navigator>
+  );
+}
